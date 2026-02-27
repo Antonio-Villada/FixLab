64de8965773648086f0ec4b1dbf2627182f393cc
AntonioVillada <antonio.viillada.7@gamil.com>
2026-02-26 19:30:45 -0500

    registro y login

---
+++ fixlab-api/src/test/java/com/software/fixlab/FixlabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.software.fixlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixlabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
