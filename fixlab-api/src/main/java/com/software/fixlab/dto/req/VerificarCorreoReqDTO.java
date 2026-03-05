c651fcb4ceb805973e7ff33f1414b733851c02ff
AntonioVillada <antonio.viillada.7@gamil.com>
2026-03-04 21:07:52 -0500

    cambias drasticos

--- fixlab-api/src/main/java/com/software/fixlab/dto/req/VerificarCorreoReqDTO.java
+++
@@ -1,8 +0,0 @@
-package com.software.fixlab.dto.req;
-import lombok.Data;
-
-@Data
-public class VerificarCorreoReqDTO {
-    private String email;
-    private String codigo;
-}
--- fixlab-api/src/main/java/com/software/fixlab/dto/req/WompiDataDTO.java
+++
@@ -1,9 +0,0 @@
-package com.software.fixlab.dto.req;
-
-import lombok.Data;
-import java.util.Map;
-
-@Data
-public class WompiDataDTO {
-    private WompiTransactionDTO transaction;
-}
